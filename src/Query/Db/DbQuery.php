97b84f336553f13dddbacb83ae98a0a652c83c7e
Seçgin ŞANLI <secgin@Secgin-MacBook-Pro.local>
2023-02-05 22:45:13 +0300

    DbQuery sınıfına  annotation tanımı eklendi

--- src/Query/Db/DbQuery.php
+++ src/Query/Db/DbQuery.php
@@ -4,7 +4,10 @@ namespace YG\Phalcon\Cqrs\Query\Db;
 
 use YG\Phalcon\Cqrs\Query\Query;
 
+
+/**
+ * @property \Phalcon\Mvc\Model\Manager|\Phalcon\Mvc\Model\ManagerInterface $modelsManager
+ */
 abstract class DbQuery extends Query
 {
-
 }
